05e564741cc49ababe3d0bde8857b3b9288b066f
AnActualEndlessVoid <42186898+AnActualEndlessVoid@users.noreply.github.com>
2018-10-17 21:33:32 -0500

    c o d e c o  d e  c o d e

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+ * This program was made to get the length of a string, without the actual function
+ *
+ * Mario Emilio Aguilar Chung
+ * A01411210@itesm.mx
+ * 10/17/2018
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char arr [1000];
+    int i;
+    int count = 0;
+    //Get the characters from the user
+    printf ("Give me a series of characters:\n");
+    printf ("\n");
+    fgets (arr,sizeof(arr),stdin);
+
+    //Each time a character is read, it will be added to the count
+    for (i = 0; i < strlen(arr); i++)
+    {
+        count++;
+    }
+    //Print the results
+    printf ("\nThere are %i characters in the string\n", count);
+    return 0;
+}
